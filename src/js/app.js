4fef342a784e9888b613bceaea779e716a8ccd82
Alex <alexandr.davyidkin@eldorado.ru>
2020-06-25 19:06:34 +0500

    1. Lint Правка

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,8 @@ import GameSavingLoader from './components/GameSavingLoader';
 (async () => {
   try {
     const game = await GameSavingLoader.load();
-    console.log(game);
     return game;
   } catch (error) {
-    console.log(error);
     return error;
   }
 })();
-
-// GameSavingLoader.load().then((saving) => {
-//   console.log(saving);
-// }, (error) => {
-//   console.log(error);
-// });

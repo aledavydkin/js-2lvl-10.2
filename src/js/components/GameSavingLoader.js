7d1035cd49001c1e6b1a4c5a30b014574c89af02
Alex <alexandr.davyidkin@eldorado.ru>
2020-06-26 16:31:26 +0500

    1. Упрощение кода

--- src/js/components/GameSavingLoader.js
+++ src/js/components/GameSavingLoader.js
@@ -3,18 +3,14 @@ import read from './reader';
 import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
-  static load() {
-    return new Promise((resolve, reject) => {
-      (async () => {
-        try {
-          const response = await read();
-          const value = await json(response);
-          const gameSaving = new GameSaving(value);
-          resolve(gameSaving);
-        } catch (error) {
-          reject(error);
-        }
-      })();
-    });
+  static async load() {
+    try {
+      const response = await read();
+      const value = await json(response);
+      const result = new GameSaving(value);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 }
